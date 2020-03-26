11c812f867cd2d921297b4adefb2a3e65ed3c44c
maxolee23 <maxolee23@gmail.com>
2020-03-26 13:35:14 -0500

    initial commit

---
+++ pages/about.js
@@ -0,0 +1,13 @@
+import Link from "next/link";
+import Layout from "../components/Layout";
+
+const About = () => (
+  <Layout>
+    <div>
+      <h1>About BitPrice</h1>
+      <p>Check out prices of BitCoin with the dropdown menu.</p>
+    </div>
+  </Layout>
+);
+
+export default About;
